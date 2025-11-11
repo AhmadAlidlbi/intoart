eb06ee15dcb6198ec11c7b6ede60bf8a28405c61
Ahmad Alidlbi <ahmedidlbi@hotmail.com>
2025-11-11 22:14:58 +0300

    Enhance navbar and footer components

--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrls: ['./navbar.scss']
 })
 export class Navbar {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss'],
 })
 export class Home {
-
+  projects = [
+    {
+      title: 'Luxury Apartment',
+      image: 'assets/images/project1.jpg',
+      description: 'Minimal luxury apartment design in Kuwait City.',
+    },
+    {
+      title: 'Coastal Villa',
+      image: 'assets/images/project2.jpg',
+      description: 'Elegant beachside villa with natural tones and textures.',
+    },
+    {
+      title: 'Modern Office',
+      image: 'assets/images/project3.jpg',
+      description: 'Creative workspace balancing art and productivity.',
+    },
+  ];
 }
